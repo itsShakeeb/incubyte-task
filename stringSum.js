4491da5a0388d57861b4e3fe7c0dcc43a1d0fd39
itsShakeeb <shakeebadab50@gmail.com>
2024-09-24 23:08:18 +0530

    handle negative number with msg

--- stringSum.js
+++ stringSum.js
@@ -1,12 +1,15 @@
+export const negate_msg = "negative number not allowed";
 export function stringSum(string) {
+  const negatives = [];
   let result = 0;
   if (string === "") return 0;
   result = string.split(",").reduce((acc, curr) => {
     const current = Number(curr);
+    if (current < 0) negatives.push(curr);
     return acc + current;
   }, 0);
-  return result;
+  return negatives.length ? `${negate_msg} ${negatives.join(",")}` : result;
 }
 
-const result = stringSum("");
+const result = stringSum("-1,2,3,-4");
 console.log("result", result);
--- stringSum.test.js
+++ stringSum.test.js
@@ -1,4 +1,4 @@
-import { stringSum } from "./stringSum.js";
+import { negate_msg, stringSum } from "./stringSum.js";
 
 describe("Sum of strings", () => {
   test("If empty String return sum as 0", () => {
@@ -7,5 +7,9 @@ describe("Sum of strings", () => {
 
   test("Should return sums of all positive number", () => {
     expect(stringSum("2,3,4")).toBe(9);
+  });
+
+  test("Should return all negative number as string from string", () => {
+    expect(stringSum("-1,2,3,-4")).toBe(`${negate_msg} -1,-4`);
   });
 });
