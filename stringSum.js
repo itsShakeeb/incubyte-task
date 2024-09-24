b2fc7c1644f25a3eff39271ec7732e6210983e03
itsShakeeb <shakeebadab50@gmail.com>
2024-09-24 22:59:20 +0530

    sum of all the number inside string

--- stringSum.js
+++ stringSum.js
@@ -1,6 +1,10 @@
 export function stringSum(string) {
   let result = 0;
   if (string === "") return 0;
+  result = string.split(",").reduce((acc, curr) => {
+    const current = Number(curr);
+    return acc + current;
+  }, 0);
   return result;
 }
 
--- stringSum.test.js
+++ stringSum.test.js
@@ -4,4 +4,8 @@ describe("Sum of strings", () => {
   test("If empty String return sum as 0", () => {
     expect(stringSum("")).toBe(0);
   });
+
+  test("Should return sums of all positive number", () => {
+    expect(stringSum("2,3,4")).toBe(9);
+  });
 });
